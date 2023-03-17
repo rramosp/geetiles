1e65660c09c95bc356f0b56c0ab93b0a0963ebf8
raul ramos <rramosp@unal.edu.co>
2023-03-17 10:00:18 -0500

    description

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ from setuptools import setup
 
 setup(name='geetiles',
       description="download Google Earth Engine datasets to tiles as geotiff arrays",
+      long_description_content_type='text/markdown',
       install_requires=['matplotlib','numpy', 'pandas','joblib',
                         'progressbar2', 'psutil', 'scipy', 'shapely',
                         'geopandas', 'pyproj', 'rasterio'
